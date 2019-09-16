a685a3a47892198facbfb97f56057c699a8338f9
Daniel Marzullo <dmarzull@ITS.JNJ.com>
2019-09-15 22:40:54 -0400

    Standardize 2 space indents

--- src/components/console.js
+++ src/components/console.js
@@ -6,54 +6,53 @@ import StrikePanel from './console/strikePanel'
 const { ipcRenderer } = window.require('electron');
 
 class Console extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props)
 
-        var gameData = require('../res/surveys.json')
-        var survey = gameData.surveys[0]
-        this.resetSurvey(survey)
+		var gameData = require('../res/surveys.json')
+		var survey = gameData.surveys[0]
+		this.resetSurvey(survey)
 
-        this.state = {
-            survey: survey,
-            scores: [
-                {name:'', score:0},
-                {name:'', score:0}
-            ]
-        }
-    }
+		this.state = {
+			survey: survey,
+			scores: [
+				{name:'', score:0},
+				{name:'', score:0}
+			]
+		}
+	}
 
-    updateSurvey = (answers) => {
-        this.setState({survey: {...this.state.survey, answers: answers}})
-        this.updateBoard()
-    }
+	updateSurvey = (answers) => {
+		this.setState({survey: {...this.state.survey, answers: answers}})
+		this.updateBoard()
+	}
 
-    updateBoard = () => {
-        ipcRenderer.send('updateBoard', this.state)
-    }
+	updateBoard = () => {
+		ipcRenderer.send('updateBoard', this.state)
+	}
 
-    resetSurvey(currentGame) {
-        currentGame.answers.forEach(function(answer) {
-            answer.revealed = false;
-        })
-    }
+	resetSurvey(currentGame) {
+		currentGame.answers.forEach(function(answer) {
+			answer.revealed = false;
+		})
+	}
 
-    render() {
-        return (
-            <div style={{height:"100%"}}> {/*necessary div for KeyboardEventHandler*/}
-                Judge Panel
-                <Board 
-                    survey={this.state.survey.answers} 
-                    admin={true}
-                    updateSurvey={this.updateSurvey}
-                    keyHandler={this.props.keyHandler}/>
-                <StrikePanel/>
-                {/* <KeyboardEventHandler
-                    handleKeys={['all']}
-                    onKeyEvent={(key, e) => this.keyHandler(key)}/> */}
-            </div>
-        )
-    }
-
+	render() {
+		return (
+			<div style={{height:"100%"}}> {/*necessary div for KeyboardEventHandler*/}
+				Judge Panel
+				<Board 
+					survey={this.state.survey.answers} 
+					admin={true}
+					updateSurvey={this.updateSurvey}
+					keyHandler={this.props.keyHandler}/>
+				<StrikePanel/>
+				{/* <KeyboardEventHandler
+						handleKeys={['all']}
+						onKeyEvent={(key, e) => this.keyHandler(key)}/> */}
+			</div>
+		)
+	}
 }
 
 export default Console
--- src/components/console/strikePanel.js
+++ src/components/console/strikePanel.js
@@ -6,38 +6,38 @@ import strike from '../../res/strike.png'
 const { ipcRenderer } = window.require('electron');
 
 export default class StrikePanel extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {enabled: true}
+  constructor(props) {
+    super(props)
+    this.state = {enabled: true}
+  }
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+  
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  showStrikes = (strike) => {
+    if (this.state.enabled) {
+      this.setState({enabled: false})
+      ipcRenderer.send('showStrikes', strike)
+      setTimeout(() => {this.setState({enabled: true})}, 1500)
     }
+  }
 
-    showModal = () => {
-        this.setState({ show: true });
-    };
-    
-    hideModal = () => {
-        this.setState({ show: false });
-    };
+  renderStrikes = (strikes) => {
+    return [...Array(strikes)].map((e, i) => <img key={i} className='strike-button' onClick={() => this.showStrikes(i+1)} src={strike} alt='X'/>)
+  }
 
-    showStrikes = (strike) => {
-        if (this.state.enabled) {
-            this.setState({enabled: false})
-            ipcRenderer.send('showStrikes', strike)
-            setTimeout(() => {this.setState({enabled: true})}, 1500)
-        }
-    }
+  render() {
+    const strikes = this.renderStrikes(3);
 
-    renderStrikes = (strikes) => {
-        return [...Array(strikes)].map((e, i) => <img key={i} className='strike-button' onClick={() => this.showStrikes(i+1)} src={strike} alt='X'/>)
-    }
-
-    render() {
-        const strikes = this.renderStrikes(3);
-
-        return (
-            <div className='strike-button-panel'>
-                {strikes}
-            </div>
-        )
-    }
+    return (
+      <div className='strike-button-panel'>
+          {strikes}
+      </div>
+    )
+  }
 }
